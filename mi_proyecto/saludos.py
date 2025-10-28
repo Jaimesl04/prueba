7122ab6426a016f27de876ea03f089b7964dc435
Jaimesl04 <jaime.de.luengo.al@iespoligonosur.org>
2025-10-28 17:22:52 +0100

    feat: añade saludo informal en español

--- mi_proyecto/saludos.py
+++ mi_proyecto/saludos.py
@@ -1,8 +1,8 @@
 # saludos.py
 from utils.printer import print_message
 def saludo(name: str):
-    """Genera un saludo simple."""
-    message = f"Hola, {name}!"
+    """Genera un saludo informal."""
+    message = f"¡Hey {name}! ¿Qué tal?"
     print_message(message)
     
 def greet_in_english(name: str):
